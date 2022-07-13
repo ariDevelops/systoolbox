9a6f0e8969608bebed24f96db4bfc38cec7f4e45
Ariel.Vieira <101485612+ariDevelops@users.noreply.github.com>
2022-07-13 12:13:56 -0300

    Update and rename getversion.pl to getOS.pl

--- getOS.pl
+++ getOS.pl
@@ -8,16 +8,17 @@ use Cwd;
 use Tk;
 #Avaiable colors for Term's lib => black  red  green  yellow  blue  magenta  cyan  white
 
-
+#search for wht system you are using
 open(my $in, "uname -ov|");
 my $sys = <$in>;
+
 $sys = "Ubuntu" if $sys =~ /\b([#]\d{2}[-])?[Uu]buntu\b/;
 $sys = "Debian" if $sys =~ /\b([#]\d{2}[-])?[Dd]ebian\b/;
 $sys = "Arch-linux" if $sys =~ /\b([#]\d{2}[-])?[Aa]rch[-._]?[Ll]inux\b/;
 $sys = "Fedora" if $sys =~ /\b([#]\d{2}[-])?([Ff]edora)\b/;
 
 
-print "sys is: $sys \n";
+print color("yellow"),"sys is: ", color("reset"), "$sys \n";
 
 if ($sys =~ /([Dd]ebian|[Uu]buntu)/) {
 my $run = system("sudo apt-install git");
